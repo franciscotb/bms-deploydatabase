a459815ce496c463fffc5d1951047d8d50be0f56
Francisco Bianchi <72532804+franciscotb@users.noreply.github.com>
2024-02-03 16:25:42 -0600

    Add files via upload

--- load_central_config.sql
+++ load_central_config.sql
@@ -12,3 +12,100 @@ INSERT INTO administration.profile(
     'Global Access'
 );
 
+INSERT INTO administration.page(
+    page_key,
+    name,
+    menu_name,
+    profile_key
+) values (
+    nextval('page_key_seq'),
+    'Cliente',
+    'Menu 1',
+    1
+);
+
+INSERT INTO administration.page(
+    page_key,
+    name,
+    menu_name,
+    profile_key
+) values (
+    nextval('page_key_seq'),
+    'Pedidos',
+    'Menu 2',
+    1
+);
+
+INSERT INTO administration.site(
+    site_key,
+    name,
+    address,
+    city,
+    state,
+    country,
+    document,
+    phone,
+    social_media,
+    license_alert,
+    creation_date,
+    discount,
+    profile_key
+) values (
+    nextval('site_key_seq'),
+    'BMS System',
+    'Rua X, 123',
+    'Campinas',
+    'Sao Paulo',
+    'Brasil',
+    '',
+    '',
+    '',
+    10,
+    now(),
+    0,
+    1
+);
+
+INSERT INTO administration.user(
+    user_key,
+    login,
+    name,
+    email,
+    phone,
+    phone2,
+    status,
+    active_date,
+    site_key
+) values (
+    nextval('user_key_seq'),
+    'carlos.martins',
+    'Carlos Martins',
+    'cmartins.dev@gmail.com',
+    '+55 19 98152-0912',
+    '',
+    'Active',
+    now(),
+    1
+);
+
+INSERT INTO administration.user(
+    user_key,
+    login,
+    name,
+    email,
+    phone,
+    phone2,
+    status,
+    active_date,
+    site_key
+) values (
+    nextval('user_key_seq'),
+    'francisco.bianchi',
+    'Francisco Bianchi',
+    'franciscotb@gmail.com',
+    '+1 256 529-3935',
+    '',
+    'Active',
+    now(),
+    1
+);
