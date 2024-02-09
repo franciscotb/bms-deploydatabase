e36d7e7a228959cd8fbbb222bab7db7f2c56c1bc
Francisco Bianchi <72532804+franciscotb@users.noreply.github.com>
2024-02-08 20:56:30 -0600

    Add files via upload

--- central_config.sql
+++ central_config.sql
@@ -15,60 +15,66 @@ ALTER DATABASE central_config SET search_path TO "user", administration, public;
 
 CREATE TABLE administration.user (
     user_key bigint NOT NULL,
-    login text NOT NULL,
+    username text NOT NULL,
     name text NOT NULL,
     email text,
     phone text,
     phone2 text,
     status text DEFAULT 'ACTIVE'::text,
-    active_date timestamp with time zone,
-    site_key bigint
+    type text,
+    creation_date timestamp with time zone,
+    parent_user_key bigint
 );
 
 -- COMMENT ON TABLE adminitration.user IS 'User details';
 -- COMMENT ON COLUMN adminitration.user.user_key IS 'Primary Key';
 
-CREATE TABLE administration.site (
-    site_key bigint NOT NULL,
-    name text NOT NULL,
+CREATE TABLE administration.user_details (
+    user_details_key bigint NOT NULL,
     address text,
     city text,
     state text,
-    country text DEFAULT 'BRASIL'::text,
+    country text,
+    timezone text,
     document text,
-    phone text,
-    email text,
     social_media text,
     logo bytea,
-    license_alert int,
-    creation_date timestamp with time zone,
-    discount int,
-    profile_key bigint
+    user_key bigint NOT NULL
+);
+
+CREATE TABLE secret_manager (
+    secret_manager_key bigint NOT NULL,
+    salt text,
+    passhash text,
+    active boolean,
+    expire_date timestamp with time zone,
+    user_key bigint NOT NULL
 );
 
 CREATE TABLE administration.profile (
     profile_key bigint NOT NULL,
-    name text,
-    price double precision DEFAULT 0,
-    number_users int,
-    profile_details text
+    name text NOT NULL,
+    description text NOT NULL,
+    enable boolean DEFAULT FALSE,
+    price double precision,
+    user_key bigint NOT NULL
 );
 
 CREATE TABLE administration.license (
     license_key bigint NOT NULL,
-    license_number text,
-    creation_date timestamp with time zone,
+    license_number text NOT NULL,
+    aquisition_date timestamp with time zone,
     expire_date timestamp with time zone,
-    cost double precision DEFAULT 0,
-    site_key bigint,
-    profile_key bigint
+    price double precision,
+    user_key bigint NOT NULL,
+    profile_key bigint NOT NULL
 );
 
 CREATE TABLE administration.page (
     page_key bigint NOT NULL,
-    name text,
-    menu_name text,
-    profile_key bigint
+    name text NOT NULL,
+    menu_name text NOT NULL,
+    profile_key bigint NOT NULL
 );
 
 CREATE TABLE administration.audit (
@@ -85,8 +91,11 @@ CREATE TABLE administration.audit (
 ALTER TABLE ONLY administration."user"
     ADD CONSTRAINT user_pkey PRIMARY KEY (user_key);
 
-ALTER TABLE ONLY administration."site"
-    ADD CONSTRAINT site_pkey PRIMARY KEY (site_key);
+ALTER TABLE ONLY administration."user_details"
+    ADD CONSTRAINT user_details_pkey PRIMARY KEY (user_details_key);
+
+ALTER TABLE ONLY administration."secret_manager"
+    ADD CONSTRAINT secret_manager_pkey PRIMARY KEY (secret_manager_key);
 
 ALTER TABLE ONLY administration."profile"
     ADD CONSTRAINT profile_pkey PRIMARY KEY (profile_key);
@@ -98,25 +107,26 @@ ALTER TABLE ONLY administration."page"
     ADD CONSTRAINT page_pkey PRIMARY KEY (page_key);
 
 -- Foreign KEYs
-ALTER TABLE ONLY administration."user"
-    ADD CONSTRAINT user_site_fkey FOREIGN KEY (site_key) REFERENCES site(site_key) DEFERRABLE INITIALLY IMMEDIATE;
+ALTER TABLE ONLY administration."user_details"
+    ADD CONSTRAINT user_details_user_fkey FOREIGN KEY (user_key) REFERENCES administration.user(user_key) DEFERRABLE INITIALLY IMMEDIATE;
 
-ALTER TABLE ONLY administration."site"
-    ADD CONSTRAINT site_profile_fkey FOREIGN KEY (profile_key) REFERENCES profile(profile_key) DEFERRABLE INITIALLY IMMEDIATE;
+ALTER TABLE ONLY administration."secret_manager"
+    ADD CONSTRAINT secret_manager_user_fkey FOREIGN KEY (user_key) REFERENCES administration.user(user_key) DEFERRABLE INITIALLY IMMEDIATE;
+
+ALTER TABLE ONLY administration."license"
+    ADD CONSTRAINT license_user_fkey FOREIGN KEY (user_key) REFERENCES administration.user(user_key) DEFERRABLE INITIALLY IMMEDIATE;
+
+ALTER TABLE ONLY administration."license"
+    ADD CONSTRAINT license_profile_fkey FOREIGN KEY (profile_key) REFERENCES administration.profile(profile_key) DEFERRABLE INITIALLY IMMEDIATE;
 
 ALTER TABLE ONLY administration."page"
-    ADD CONSTRAINT page_profile_fkey FOREIGN KEY (profile_key) REFERENCES profile(profile_key) DEFERRABLE INITIALLY IMMEDIATE;
-
-ALTER TABLE ONLY administration."license"
-    ADD CONSTRAINT license_site_fkey FOREIGN KEY (site_key) REFERENCES site(site_key) DEFERRABLE INITIALLY IMMEDIATE;
-
-ALTER TABLE ONLY administration."license"
-    ADD CONSTRAINT license_profile_fkey FOREIGN KEY (profile_key) REFERENCES profile(profile_key) DEFERRABLE INITIALLY IMMEDIATE;
+    ADD CONSTRAINT page_profile_fkey FOREIGN KEY (profile_key) REFERENCES administration.profile(profile_key) DEFERRABLE INITIALLY IMMEDIATE;
 
 CREATE INDEX user_user_key_idx ON administration.user USING btree (user_key);
-CREATE INDEX site_site_key_idx ON administration.site USING btree (site_key);
-CREATE INDEX profile_profile_key_idx ON administration.profile USING btree (profile_key);
+CREATE INDEX user_details_user_details_key_idx ON administration.user_details USING btree (user_details_key);
+CREATE INDEX secret_manager_secret_manager_key_idx ON administration.secret_manager USING btree (secret_manager_key);
 CREATE INDEX license_license_key_idx ON administration.license USING btree (license_key);
+CREATE INDEX profile_profile_key_idx ON administration.profile USING btree (user_key);
 CREATE INDEX page_page_key_idx ON administration.page USING btree (page_key);
 CREATE INDEX audit_audit_key_idx ON administration.audit USING btree (audit_key);
 
@@ -127,7 +137,14 @@ CREATE SEQUENCE administration.user_key_seq
     NO MAXVALUE
     CACHE 1;
 
-CREATE SEQUENCE administration.site_key_seq
+CREATE SEQUENCE administration.user_details_key_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+CREATE SEQUENCE administration.secret_manager_key_seq
     START WITH 1
     INCREMENT BY 1
     NO MINVALUE
